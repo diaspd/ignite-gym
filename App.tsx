b317ac95d77d58005373e06505b0ad16de5dca9a
Pedro Dias <pdr050203@gmail.com>
2024-04-17 21:27:32 -0300

    add: theme

--- App.tsx
+++ App.tsx
@@ -3,7 +3,8 @@ import { View, StatusBar } from 'react-native';
 import { Box, NativeBaseProvider } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { Loading } from '@components/components';
+import { THEME } from './src/theme'
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({
@@ -12,7 +13,7 @@ export default function App() {
   })
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar 
         barStyle="light-content"
         backgroundColor='transparent'
