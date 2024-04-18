9e4abc2180e6deaf91fdbfc693c3c4864dfa87ff
Pedro Dias <pdr050203@gmail.com>
2024-04-17 22:38:25 -0300

    add: image to background on sign in page

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import { View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 
-import { Box, NativeBaseProvider } from 'native-base'
+import { NativeBaseProvider } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { THEME } from './src/theme'
 import { Loading } from '@components/Loading';
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({
@@ -20,7 +21,7 @@ export default function App() {
         translucent
       />
       
-      {fontsLoaded ? <View /> : <Loading />}
+      {fontsLoaded ? <SignIn /> : <Loading />}
 
     </NativeBaseProvider>
   );
