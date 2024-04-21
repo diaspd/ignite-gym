3f43117735e7de3f0fe4afeeded810f1780e68c3
Pedro Dias <pdr050203@gmail.com>
2024-04-21 14:41:52 -0300

    add: routes to app

--- App.tsx
+++ App.tsx
@@ -3,9 +3,10 @@ import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { Routes } from '@routes/index';
+
 import { THEME } from './src/theme'
 import { Loading } from '@components/Loading';
-import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({
@@ -21,7 +22,7 @@ export default function App() {
         translucent
       />
       
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
 
     </NativeBaseProvider>
   );
