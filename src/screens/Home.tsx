8018f389b9ff7f7fe73145f839453e72f35bc082
Pedro Dias <pdr050203@gmail.com>
2024-04-23 23:41:41 -0300

    add: muscular groups component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,33 @@
+import { useState } from 'react'
+
+import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-import { VStack } from 'native-base'
+import { HStack, VStack, FlatList } from 'native-base'
 
 export function Home() {
+  const [groups, setGroups] = useState(["Costas", "Bícepes", "Trícepes", "Ombro"]);
+  const [isGroupSelected, setIsGroupSelected] = useState("costas")
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+    
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group 
+          name={item} 
+          isActive={isGroupSelected === item} 
+          onPress={() => setIsGroupSelected(item)}
+        />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   )
 }
