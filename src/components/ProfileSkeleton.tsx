c502378ac75c2d9726b2035fca1f9a71888b57cf
Pedro Dias <pdr050203@gmail.com>
2024-04-25 23:40:09 -0300

    add: profile layout skeleton

--- src/components/ProfileSkeleton.tsx
+++ src/components/ProfileSkeleton.tsx
@@ -12,6 +12,82 @@ export function ProfileSkeleton() {
         startColor="gray.500"
         endColor="gray.400"
       />
+
+      <Skeleton 
+        w={20}
+        h={4}
+        mt={4} 
+        mb={8}
+        startColor="gray.500"
+        endColor="gray.400"
+        rounded="md"
+      />
+
+      <Skeleton 
+        w={342}
+        h={14}
+        startColor="gray.500"
+        endColor="gray.400"
+        rounded="sm"
+      />
+
+      <Skeleton 
+        w={342}
+        mt={3}
+        h={14}
+        startColor="gray.500"
+        endColor="gray.400"
+        rounded="sm"
+      />
+
+      <Skeleton 
+        w={24}
+        h={6}
+        mt={16} 
+        mb={1}
+        startColor="gray.500"
+        endColor="gray.400"
+        rounded="md"
+        ml={-56}
+        mr={6} 
+      />
+      
+      <Skeleton 
+        w={342}
+        mt={3}
+        h={14}
+        startColor="gray.500"
+        endColor="gray.400"
+        rounded="sm"
+      />
+
+      <Skeleton 
+        w={342}
+        mt={3}
+        h={14}
+        startColor="gray.500"
+        endColor="gray.400"
+        rounded="sm"
+      />
+
+      <Skeleton 
+        w={342}
+        mt={3}
+        h={14}
+        startColor="gray.500"
+        endColor="gray.400"
+        rounded="sm"
+      />
+
+      <Skeleton 
+        w={342}
+        mt={8}
+        h={14}
+        startColor="gray.500"
+        endColor="gray.400"
+        rounded="sm"
+        mb={8}
+      />
     </>
   )
 }
