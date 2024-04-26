b3b09bb9f58780db9b7c4006cf072f360ab78250
Pedro Dias <pdr050203@gmail.com>
2024-04-26 20:57:43 -0300

    add: header to Exercise screen

--- src/components/ProfileSkeleton.tsx
+++ src/components/ProfileSkeleton.tsx
@@ -17,28 +17,24 @@ export function ProfileSkeleton() {
         w={20}
         h={4}
         mt={4} 
-        mb={8}
+        mb={6}
         startColor="gray.500"
         endColor="gray.400"
         rounded="md"
       />
 
-      <Skeleton 
-        w={342}
-        h={14}
-        startColor="gray.500"
-        endColor="gray.400"
-        rounded="sm"
-      />
-
-      <Skeleton 
-        w={342}
-        mt={3}
-        h={14}
-        startColor="gray.500"
-        endColor="gray.400"
-        rounded="sm"
-      />
+      {Array.from({ length: 2 }).map((_, i) => {
+        return (
+          <Skeleton 
+          w={342}
+          my={2}
+          h={14}
+          startColor="gray.500"
+          endColor="gray.400"
+          rounded="sm"
+        />
+        )
+      })}
 
       <Skeleton 
         w={24}
@@ -51,32 +47,19 @@ export function ProfileSkeleton() {
         mr={6} 
       />
       
-      <Skeleton 
-        w={342}
-        mt={3}
-        h={14}
-        startColor="gray.500"
-        endColor="gray.400"
-        rounded="sm"
-      />
-
-      <Skeleton 
-        w={342}
-        mt={3}
-        h={14}
-        startColor="gray.500"
-        endColor="gray.400"
-        rounded="sm"
-      />
-
-      <Skeleton 
-        w={342}
-        mt={3}
-        h={14}
-        startColor="gray.500"
-        endColor="gray.400"
-        rounded="sm"
-      />
+      {Array.from({ length: 3 }).map((_, i) => {
+        return (
+          <Skeleton 
+            key={i}
+            w={342}
+            mt={3}
+            h={14}
+            startColor="gray.500"
+            endColor="gray.400"
+            rounded="sm"
+          />
+        )
+      })}
 
       <Skeleton 
         w={342}
--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,11 +1,45 @@
-import { Center, Text } from 'native-base'
+import { TouchableOpacity } from 'react-native'
+import { HStack, Heading, Icon, VStack, Text } from 'native-base'
+import { Feather } from '@expo/vector-icons'
+
+import { useNavigation } from '@react-navigation/native'
+import { AppNavigatorRoutesProps } from '@routes/app.routes'
+
+import BodySvg from '@assets/images/body.svg';
 
 export function Exercise() {
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  function handleGoBack() {
+    navigation.goBack()
+  }
+
   return (
-    <Center flex={1}>
-      <Text color="white">
-        Exercise
-      </Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack px={8} bg="gray.600" pt={12}>
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon 
+            as={Feather}
+            name='arrow-left'
+            color="green.500"
+            size={6}
+          />
+        </TouchableOpacity>
+
+        <HStack justifyContent="space-between" mt={4} mb={8} alignItems="center">
+          <Heading color="gray.100" fontSize="lg"  flexShrink={1}>
+            Puxada frontal
+          </Heading>
+
+          <HStack alignItems="center">
+            <BodySvg />
+
+            <Text color="gray.200" ml={1} textTransform="capitalize">
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
+      </VStack>
+    </VStack>
   )
 }
