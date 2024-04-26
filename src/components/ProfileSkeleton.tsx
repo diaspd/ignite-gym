19e3aa24779c1096cbc58df8b6e0a3dd30550623
Pedro Dias <pdr050203@gmail.com>
2024-04-25 23:00:06 -0300

    add: skeleton screen for photo on profile

---
+++ src/components/ProfileSkeleton.tsx
@@ -0,0 +1,15 @@
+import { Skeleton } from "native-base";
+
+import { PHOTO_SIZE } from "@screens/Profile";
+
+export function ProfileSkeleton() {
+  return (
+    <Skeleton 
+      w={PHOTO_SIZE} 
+      h={PHOTO_SIZE} 
+      rounded="full" 
+      startColor="gray.500"
+      endColor="gray.400"
+    />
+  )
+}
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,11 +1,31 @@
-import { Center, Text } from 'native-base'
+import { Center, ScrollView, VStack, Skeleton } from 'native-base'
+import { ScreenHeader } from '@components/ScreenHeader'
+import { UserPhoto } from '@components/UserPhoto'
+import { useState } from 'react'
+import { ProfileSkeleton } from '@components/ProfileSkeleton'
+
+export const PHOTO_SIZE = 33
 
 export function Profile() {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <Center flex={1}>
-      <Text color="white">
-        Profile
-      </Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title='Perfil'/>
+      <ScrollView>
+        <Center mt={6} px={10}>
+          {isLoading ? (
+            <ProfileSkeleton />
+          ): (
+            <UserPhoto 
+              source={{ uri: 'https://github.com/diaspd.png'}}
+              alt="Foto do usuário"
+              size={PHOTO_SIZE}
+            />
+          )}
+        </Center>
+
+      </ScrollView>
+    </VStack>
   )
 }
