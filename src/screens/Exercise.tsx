fdec86cc098d2e1a2fc24a59d603c89f4fe1c153
Pedro Dias <pdr050203@gmail.com>
2024-04-26 21:21:55 -0300

    ci: finish exercise layout

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,11 +1,15 @@
 import { TouchableOpacity } from 'react-native'
-import { HStack, Heading, Icon, VStack, Text } from 'native-base'
+import { HStack, Heading, Icon, Image, VStack, Text, Box } from 'native-base'
 import { Feather } from '@expo/vector-icons'
 
 import { useNavigation } from '@react-navigation/native'
 import { AppNavigatorRoutesProps } from '@routes/app.routes'
 
 import BodySvg from '@assets/images/body.svg';
+import SeriesSvg from '@assets/images/series.svg';
+import RepetitionsSvg  from '@assets/images/repetitions.svg';
+
+import { Button } from '@components/Button';
 
 export function Exercise() {
   const navigation = useNavigation<AppNavigatorRoutesProps>()
@@ -39,6 +43,40 @@ export function Exercise() {
             </Text>
           </HStack>
         </HStack>
+      </VStack>
+
+      <VStack p={8}>
+        <Image
+          w="full"
+          h={80}
+          source={{ uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg' }}
+          alt="Nome do exercício"
+          mb={3}
+          resizeMode="cover"
+          rounded="lg"
+        />
+
+        <Box bg="gray.600" rounded="md" pb={4} px={4}>
+          <HStack alignItems="center" justifyContent="space-around" mb={6} mt={5}>
+            <HStack>
+              <SeriesSvg />
+              <Text color="gray.200" ml="2">
+                3 séries
+              </Text>
+            </HStack>
+
+            <HStack>
+              <RepetitionsSvg />
+              <Text color="gray.200" ml="2">
+                12 repetições
+              </Text>
+            </HStack>
+          </HStack>
+
+          <Button 
+            title="Marcar como realizado"
+          />
+        </Box>
       </VStack>
     </VStack>
   )
