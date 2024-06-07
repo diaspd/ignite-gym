a39f3d86d376f527bb4c0bab567c9e1a47ed3a27
Pedro Dias <pdr050203@gmail.com>
2024-06-07 00:41:10 -0300

    fix: text 'response'

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -6,7 +6,7 @@ const api = axios.create({
 })
 
 api.interceptors.response.use((response) => response, (error) => {
-  if (error.resonse && error.response.data) {
+  if (error.response && error.response.data) {
     return Promise.reject(new AppError(error.response.data.message))
   } else {
     return Promise.reject(error)
