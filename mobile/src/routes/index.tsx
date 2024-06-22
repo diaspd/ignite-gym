1bb8ab04c12c37ba838faff0ddbf91efa66e9a51
Pedro Dias <pdr050203@gmail.com>
2024-06-21 22:43:12 -0300

    add: useAuth hook,

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,17 +1,15 @@
-import { useContext } from 'react';
 import { useTheme, Box } from 'native-base'
 
-import { AuthContext } from '@contexts/AuthContext';
+import { useAuth } from '@hooks/useAuth';
 
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { AuthRoutes } from "./auth.routes";
-import { AppRoutes } from "./app.routes";
 
 export function Routes() {
   const { colors } = useTheme();
+  const { user } = useAuth()
   
-  const contextData = useContext(AuthContext);
-  console.log(contextData)
+  console.log('', user)
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700]
