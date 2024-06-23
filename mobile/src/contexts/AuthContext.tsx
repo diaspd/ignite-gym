16eed97d15d8adf525b17d89b9ff6c06da17f68b
Pedro Dias <pdr050203@gmail.com>
2024-06-22 22:22:50 -0300

    add: state to save context data

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, type ReactNode } from "react";
+import { createContext, useState, type ReactNode } from "react";
 
 import type { UserDTO } from "src/dto/UserDTO";
 
@@ -13,15 +13,15 @@ type AuthContextProviderProps = {
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [user, setUser] = useState({
+    id: '1',
+    name: 'Pedro',
+    email: 'pedro@email.com',
+    avatar: 'diaspd.png'
+  });
+
   return (
-    <AuthContext.Provider value={{
-      user: {
-        id: '1',
-        name: 'Pedro',
-        email: 'pedro@email.com',
-        avatar: 'diaspd.png'
-      }
-    }}>
+    <AuthContext.Provider value={{ user }}>
       {children}
     </AuthContext.Provider>
   )
