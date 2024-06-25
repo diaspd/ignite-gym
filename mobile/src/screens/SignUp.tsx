6fb99585c67dec4c563f0d9b3bb673ce36a20aa1
Pedro Dias <pdr050203@gmail.com>
2024-06-24 21:03:42 -0300

    add: create account and pushing user to home

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react'
 import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from 'native-base'
+
 import { useNavigation } from '@react-navigation/native'
 import { useForm, Controller } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
@@ -11,6 +13,7 @@ import { Input } from '@components/Input'
 import { Button } from '@components/Button'
 import { api } from '@services/api'
 
+import { useAuth } from '@hooks/useAuth'
 import { AppError } from '@utils/AppError'
 
 type FormDataProps = {
@@ -28,6 +31,10 @@ const signUpSchema = y.object({
 })
 
 export function SignUp() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { signIn } = useAuth()
+
   const toast = useToast();
 
   const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
@@ -42,9 +49,13 @@ export function SignUp() {
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
     try {
-      const response = await api.post('/users', { name, email, password});
-      console.log(response)
+      setIsLoading(true)
+      
+      await api.post('/users', { name, email, password});
+
+      await signIn(email, password)
     } catch (error) {
+      setIsLoading(false)
       const isAppError = error instanceof AppError;
 
       const title = isAppError ? error.message : 'Não foi possível criar a conta. Tente novamente mais tarde.';
@@ -142,6 +153,7 @@ export function SignUp() {
         <Button 
           title='Criar e acessar'
           onPress={handleSubmit(handleSignUp)}  
+          disabled={isLoading}
         />
       </Center>
 
