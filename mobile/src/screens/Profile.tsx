fbdfdaf32330a304c3c688ab5bedc9cdf84b58cd
Pedro Dias <pdr050203@gmail.com>
2024-07-05 18:07:58 -0300

    add: update user info

--- mobile/src/screens/Profile.tsx
+++ mobile/src/screens/Profile.tsx
@@ -12,6 +12,9 @@ import * as yup from 'yup';
 
 import { useAuth } from '@hooks/useAuth';
 
+import { api } from '@services/api';
+import { AppError } from '@utils/AppError';
+
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
 import { Input } from '@components/Input'
@@ -49,6 +52,7 @@ const profileSchema = yup.object({
 })
 
 export function Profile() {
+  const [isUpdating, setIsUpdating] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [userPhoto, setUserPhoto] = useState('https://github.com/diaspd.png')
 
@@ -106,7 +110,27 @@ export function Profile() {
   }
 
   async function handleProfileUpdate(data: FormDataProps) {
-    console.log(data)
+    try {
+      setIsUpdating(true);
+      await api.put('/users', data);
+
+      toast.show({
+        title: 'Perfil atualizado com sucesso!',
+        placement: 'top',
+        bgColor: 'green.500'
+      });
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError ? error.message : 'Não foi possível atualizar os dados. Tente novamente mais tarde.';
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    } finally {
+      setIsUpdating(false);
+    }
   }
 
   return (
@@ -261,7 +285,12 @@ export function Profile() {
               )}
             />
 
-            <Button title='Atualizar' mt={4} onPress={handleSubmit(handleProfileUpdate)}/>
+            <Button 
+              title='Atualizar' 
+              mt={4} 
+              onPress={handleSubmit(handleProfileUpdate)}
+              isLoading={isUpdating}
+            />
           </>
           )}
         </Center>
