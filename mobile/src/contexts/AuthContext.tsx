a8de1907ac76740f2aaa12efe7fc45b72b80747f
Pedro Dias <pdr050203@gmail.com>
2024-07-07 00:41:53 -0300

    ci: store refresh token

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -29,12 +29,12 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     setUser(userData); 
   }
 
-  async function storageUserAndTokenSave(userData: UserDTO, token: string) {
+  async function storageUserAndTokenSave(userData: UserDTO, token: string, refresh_token: string) {
     try {
       setIsLoadingUserStorageData(true)
      
       await storageUserSave(userData);
-      await storageAuthTokenSave(token);
+      await storageAuthTokenSave({ token, refresh_token });
 
     } catch (error) {
       throw error;
@@ -47,8 +47,8 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     try {
       const { data } = await api.post('/sessions', { email, password });
       
-      if (data.user && data.token) {
-        await storageUserAndTokenSave(data.user, data.token)
+      if(data.user && data.token && data.refresh_token) {
+        await storageUserAndTokenSave(data.user, data.token, data.refresh_token);
         userAndTokenUpdate(data.user, data.token);
       }
     } catch (error) {
@@ -85,7 +85,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       setIsLoadingUserStorageData(true);
 
       const userLogged = await storageUserGet();
-      const token = await storageAuthTokenGet();
+      const { token } = await storageAuthTokenGet();
       
       if(token && userLogged) {
         userAndTokenUpdate(userLogged, token)
--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,14 @@
+import axios, { AxiosError, AxiosInstance  } from 'axios';
 import { AppError } from '@utils/AppError';
-import axios, { type AxiosInstance } from 'axios';
+
+import { storageAuthTokenGet } from "@storage/storageAuthToken";
 
 type SignOut = () => void;
+
+type PromiseType = {
+  onSuccess: (token: string) => void;
+  onFailure: (error: AxiosError) => void;
+}
 
 type APIInstanceProps = AxiosInstance & {
   registerInterceptTokenManager: (signOut: SignOut) => () => void;
@@ -9,13 +16,19 @@ type APIInstanceProps = AxiosInstance & {
 
 const api = axios.create({
   baseURL: 'http://172.20.112.1:3333'
-})as APIInstanceProps;
-
+}) as APIInstanceProps;
 
 api.registerInterceptTokenManager = singOut => {
-  const interceptTokenManager = api.interceptors.response.use((response) => response, (requestError) => {
+  const interceptTokenManager = api.interceptors.response.use((response) => response, async (requestError) => {
     if(requestError.response?.status === 401) {
       if(requestError.response.data?.message === 'token.expired' || requestError.response.data?.message === 'token.invalid') {
+        const { refresh_token } = await storageAuthTokenGet();
+
+        if(!refresh_token) {
+          singOut();
+          return Promise.reject(requestError);
+        }
+
       }
       singOut();
     }
