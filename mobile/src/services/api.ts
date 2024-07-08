59a42592feec92f9a5a269dd24eb7c9935c2f1eb
Pedro Dias <pdr050203@gmail.com>
2024-07-08 00:02:07 -0300

    add: splash and icon to app

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -15,7 +15,7 @@ type APIInstanceProps = AxiosInstance & {
 }
 
 const api = axios.create({
-  baseURL: 'http://172.20.112.1:3333'
+  baseURL: 'http://192.168.0.111:3333'
 }) as APIInstanceProps;
 
 let failedQueued: Array<PromiseType> = [];
